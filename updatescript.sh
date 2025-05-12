4d4803f57b49a07985df3e674958f2d15d5e9906
Ruby Decker <rdecker@penda.com>
2025-05-12 13:47:35 -0500

    add email functionality

--- updatescript.sh
+++ updatescript.sh
@@ -2,4 +2,5 @@
 
 /usr/bin/sudo apt-get update
 /usr/bin/sudo apt-get -y dist-upgrade
+printf "Attached is the logfile for the weekly upload." | sudo mail -s "Weekly Update Log" -A /home/kfiadmin/updatelog.log <your_email_address>
 /usr/bin/sudo shutdown -r +5 "System rebooting in five minutes for updates."
